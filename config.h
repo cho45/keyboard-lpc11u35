979d5645b6e1b76c9655b603d794cc753a4ed462
cho45 <cho45@lowreal.net>
2018-03-02 00:14:03 +0900

    done

--- config.h
+++ config.h
@@ -3,8 +3,8 @@
 
 Serial serial(UART_TX, UART_RX);
 
-#define DEBUG 1
-#define DEBUG_KEYEVENT 1
+#define DEBUG 0
+#define DEBUG_KEYEVENT 0
 
 #if DEBUG_KEYEVENT
 #define DEBUG_PRINTF_KEYEVENT(...) serial.printf(__VA_ARGS__)
--- main.cpp
+++ main.cpp
@@ -32,36 +32,6 @@ static bool state = 0;
 
 DigitalOut led(LED1);
 
-void usb_deepsleep() {
-	//1. Set bit AP_CLK in the USBCLKCTRL register (Table 41) to 0 (default) to enable automatic control of the USB need_clock signal.
-	DEBUG_PRINTF("1\r\n");
-	LPC_SYSCON->USBCLKCTRL &= ~(1<<0);
-	//2. Wait until USB activity is suspended by polling the DSUS bit in the DSVCMD_STAT register (DSUS = 1).
-	DEBUG_PRINTF("2\r\n");
-	while ( (LPC_USB->DEVCMDSTAT & (1<<17)) == 0) { }
-	//3. The USB need_clock signal will be deasserted after another 2 ms. Poll the USBCLKST register until the USB need_clock status bit is 0 (Table 42).
-	DEBUG_PRINTF("3\r\n");
-	while (LPC_SYSCON->USBCLKST != 0) {}
-	//4. Once the USBCLKST register returns 0, enable the USB activity wake-up interrupt in the NVIC (# 30) and clear it.
-	DEBUG_PRINTF("4\r\n");
-	NVIC_EnableIRQ(USBWakeup_IRQn);
-	NVIC_ClearPendingIRQ(USBWakeup_IRQn);
-	//5. Set bit 1 in the USBCLKCTRL register to 1 to trigger the USB activity wake-up interrupt on the rising edge of the USB need_clock signal.
-	DEBUG_PRINTF("5\r\n");
-	LPC_SYSCON->USBCLKCTRL |= 1<<1;
-	//6. Enable the wake-up from Deep-sleep or Power-down modes on this interrupt by enabling the USB need_clock signal in the STARTERP1 register (Table 44, bit 19).
-	DEBUG_PRINTF("6\r\n");
-	LPC_SYSCON->STARTERP1 |= 1<<19;
-	//7. Enter Deep-sleep or Power-down modes by writing to the PCON register.
-	DEBUG_PRINTF("7\r\n");
-	LPC_PMU->PCON = 0x1;
-	//8. Execute a WFI instruction.
-	DEBUG_PRINTF("8\r\n");
-	__WFI();
-}
-
-void usbwakeup() {}
-
 int main() {
 	i2c.frequency(250000);
 
@@ -101,6 +71,7 @@ int main() {
 			}
 		}
 
+		// sleep causes USB stall
 		// sleep();
 	}
 }
